73c2c74d23331f49ebedf5a5fb04f8e9f7ac4f78
chupper100 <phamminhdat223@gmail.com>
2022-01-18 09:55:33 +0700

    Thêm functions để check các loại tam giác khác nhau.

--- main.py
+++ main.py
@@ -1,23 +1,6 @@
-#pow(co so, luy thua) hàm dùng để tính thừa số.
+from modules import *
 from time import sleep
-import sys
 
-running=False
-def tamgiacvuong():
-	running=True
-	canha=int(input('Enter A side: '))
-	canhb=int(input('Enter B side: '))
-	canhc=int(input('Enter C side: '))
-	if pow(canha,2)==pow(canhb,2)+pow(canhc,2) or pow(canhb,2)==pow(canha,2)+pow(canhc,2) or pow(canhc,2)==pow(canha,2)+pow(canhb,2):
-		print('This is a right triangle.')
-	elif pow(canha,2)!=pow(canhb,2)+pow(canhc,2) or pow(canhb,2)!=pow(canha,2)+pow(canhc,2) or pow(canhc,2)!=pow(canha,2)+pow(canhb,2):
-		print('This is not a right triangle.')
-	sleep(1)
-	running=False
-
-while running==False:    
-  user_choice=str(input('y: yes, n: no\nDo you want to check?'))
-  if user_choice == 'yes' or user_choice == 'y':
-    tamgiacvuong()
-  elif user_choice == 'no'or user_choice == 'n':
-    sys.exit('Leaving...')
+while True:
+    TamGiac()
+    sleep(1)
---
+++ modules.py
@@ -0,0 +1,50 @@
+from ast import Try
+
+
+def tamgiac(canhA, canhB, canhC):
+    if canhA + canhB > canhC and canhA + canhC > canhB and canhB + canhC > canhA:
+        return True
+
+
+def tamgiacVuong(canhA, canhB, canhC):
+    if (
+        canhA * canhA == (canhB * canhB + canhC * canhC)
+        or canhB * canhB == (canhA * canhA + canhC * canhC)
+        or canhC * canhC == (canhA * canhA + canhB * canhB)
+    ):
+        return True
+    else:
+        return "Không phải tam giác vuông."
+
+
+def tamgiacCan(canhA, canhB, canhC):
+    if ((canhA == canhB) or (canhB == canhC) or (canhC == canhA)) and not tamgiacDeu(
+        canhA, canhB, canhC
+    ):
+        return True
+    else:
+        return "Không phải tam giác cân."
+
+
+def tamgiacDeu(canhA, canhB, canhC):
+    if canhA == canhB == canhC:
+        return True
+
+
+def TamGiac():
+    canhA = int(input("Cạnh A: "))
+    canhB = int(input("Cạnh B: "))
+    canhC = int(input("Cạnh C: "))
+
+    if tamgiac(canhA, canhB, canhC):
+        functions = [tamgiacVuong, tamgiacCan, tamgiacDeu]
+        # loop through all functions
+        for function in functions:
+            result = function(canhA, canhB, canhC)
+            if result == True:
+                print(f"{function.__name__}" + ": " + str(result))
+                break
+            elif result == False:
+                print(f"{function.__name__}" + ": " + "False")
+    else:
+        print("Không phải tam giác.")
