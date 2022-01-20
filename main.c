a33e7e2f0e94f943be431eaa62f1785d5b281009
chupper100 <phamminhdat223@gmail.com>
2022-01-20 18:52:01 +0700

    c

--- main.c
+++ main.c
@@ -2,6 +2,11 @@
 
 int main()
 {
-    printf("Hello World\n");
+    char characterName[] = "Chup";
+    char characterAge = 30;
+
+    printf("Hello, %s!\n", characterName);
+    printf("He is %d years old.\n", characterAge);
+
     return 0;
 }
